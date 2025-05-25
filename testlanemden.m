5c5c007ff87937687f723cee2ebbbb394b877c39
Uwe Brauer <oub@mat.ucm.es>
2025-05-25 16:11:14 +0200

    Add PDF with my solutions, add remarks about the code

--- testlanemden.m
+++ testlanemden.m
@@ -1,5 +1,12 @@
-%%
+%% n=3
 addpath /home/oub/ALLES/HGs/TFGs/TFG-2025-Pena
 % n index de la ecuacion de Lane-Emden 
-[alfa,y]=laneemden(3,40,30)
+[alfa,y]=laneemden(3,100,10)
+% el resultado es NaN
+
+
+%% n=5
+addpath /home/oub/ALLES/HGs/TFGs/TFG-2025-Pena
+% n index de la ecuacion de Lane-Emden 
+[alfa,y]=laneemden(5,40,30)
 % el resultado es NaN
