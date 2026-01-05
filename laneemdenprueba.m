bac9448f2564e4066a2eccec3193e3e32d752c38
Diego Fernandez <diegfe03@ucm.es>
2026-01-05 16:33:13 +0100

    cambios en la tabla y en la grafica

--- laneemdenprueba.m
+++ laneemdenprueba.m
@@ -3,21 +3,21 @@
 % maxiter=input('introduce maxiter');   %Número de iteraciones
 N=40;
 maxiter=10;
-nvalores=[1,2,3,4];  %Valores del índice politrópico
+nvalores=[0,1,2,3,4,5];  %Valores del índice politrópico
 %Prepara la tabla de resultados
 resultado=zeros(maxiter,length(nvalores));
     %Calcula resultados para cada n
     for i=1:length(nvalores)
         n=nvalores(i);
-        [~,~,alfas,~,~]=laneemdenNEWTONKANTOROVICH(n,N,maxiter);
+        [~,~,alfas]=laneemdenSL(n,N,maxiter);
         resultado(:,i)=alfas;
     end
 %Muestra la tabla formateada
 disp('-------------------------------------------------------------');
-disp('| Iters | n = 1       | n = 2       | n = 3       | n = 4       |');
+disp('| Iters | n = 0       | n = 1       | n = 2       | n = 3       | n = 4    | n = 5       |');
 disp('-------------------------------------------------------------');
     for iter=1:maxiter
-        fprintf('| %-5d ',iter);
+        fprintf('| %-8d ',iter);
         for columnas=1:length(nvalores)
             fprintf('| %-20.15f ',resultado(iter,columnas));
         end
