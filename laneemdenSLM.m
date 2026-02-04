9cd63e18081f51a092d3169ae669095562a0f8c7
Diego Fernandez <diegfe03@ucm.es>
2026-02-04 14:52:32 +0100

    add the same copyright in all the files

--- laneemdenSLM.m
+++ laneemdenSLM.m
@@ -1,3 +1,17 @@
+% Copyright (C) 2026 Diego Fernandez de la Peña
+
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 function [alfa,y,alfas]=laneemdenSLM(n,N,maxiter)
 % Funcion que resuelve la ecuacion de Lane-Emden para un N mayor que 2 y un
 % n comprendido entre 0 y 5 
